6155723b7e2f1b585fc587fb3a6f7c6d29d89d16
RAMENDRA SRIVASTAVA <114799768+ramendra16@users.noreply.github.com>
2024-02-15 10:35:58 +0530

    JAVA

---
+++ Strings3.java
@@ -0,0 +1,8 @@
+public class Strings3{
+	public static void main(String args[]){
+	String name1 = "Tony";
+	String name2 = "Stark";
+	String fullName = name1 + " " +name2;
+	System.out.println("Total length of this array: " +"(" +fullName + ")"+ " is: " + fullName.length()); //CONCATENATION
+}
+}
