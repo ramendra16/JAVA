749b9119352f909aa0d45df0839762396eb6340d
RAMENDRA SRIVASTAVA <114799768+ramendra16@users.noreply.github.com>
2024-02-15 10:35:36 +0530

    JAVA

---
+++ Strings2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Strings2{
+	public static void main(String args[]){
+	Scanner sc = new Scanner(System.in);
+	String name = sc.nextLine(); //nextLine is used for taking all user input in the string. 
+	System.out.println("Your name is: "+ name);
+}
+}
