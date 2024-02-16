a908a143cd6af60263150b93e392cbf8970c7844
RAMENDRA SRIVASTAVA <114799768+ramendra16@users.noreply.github.com>
2024-02-16 10:57:40 +0530

    JAVA

---
+++ Strings8_ParseInt_Method_of_Integer_class.java
@@ -0,0 +1,16 @@
+public class Strings8_ParseInt_Method_of_Integer_class {
+    public static void main(String args[]) {
+        String str = "123";
+        String str2 = "1234";
+
+        int number1 = Integer.parseInt(str);
+        int number2 = Integer.parseInt(str2);
+
+        System.out.println(number1 + number2);
+
+
+
+
+        System.out.println();
+        }
+}
